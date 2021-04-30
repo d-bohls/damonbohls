cf47793bcd0db97cd0e90d5f212b448bf842aa49
Damon Bohls <33291082+d-bohls@users.noreply.github.com>
2021-04-30 09:39:34 -0500

    refactor: Increase # of particles a bit
    Change from 1 particle every 10,000 pixels to every 8,000 pixels.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ document.addEventListener(
       document.getElementById('particles'),
       {
         dotColor: '#5cbdaa80',
-        lineColor: '#5cbdaa80'
+        lineColor: '#5cbdaa80',
+        density: 8000,
       });
   },
   false);
